b3e83c949292214bdfe51390920c3531887c1dc3
Teya <Teya@TVOROG>
2021-12-23 03:39:21 +0200

    Добавьте файлы проекта.

---
+++ 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Random rand = new Random();
+                int[] arr = new int[17];
+                for (int i = 0; i < 17; i++)
+                {
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+                int Sum = 0;
+                for (int i = 0; i < 17; i++)
+                {
+                    Sum += arr[i];
+                }
+                Console.WriteLine("Сумма=" + Sum);
+            }
+        }
+    }
+}
